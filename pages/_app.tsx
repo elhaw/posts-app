43061e208fd03c13d18ee00d779322b4ff14d9a3
elhaw <abd.elsayed@hotmail.com>
2022-02-11 16:43:54 +0200

    imported Layout and fixed css issue (make sure min content take full browser height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Layout } from '@modules/shared/components/organisms'
+import '../styles/globals.css'
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        #__next {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+        #__next main {
+          flex: 1;
+          overflow-x: hidden;
+          background-color: white;
+        }
+      `}</style>
+    </Layout>
+  )
+}
