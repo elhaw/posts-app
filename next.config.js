8496ee1f6a5ab23a046d7a50b801f9c887cadcfd
elhaw <abd.elsayed@hotmail.com>
2022-02-11 16:10:32 +0200

    added images array domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: [
+    'https://img.dummyapi.io',
+    'https://dummyapi.io',
+    'https://randomuser.me',
+  ],
 }
 
 module.exports = nextConfig
