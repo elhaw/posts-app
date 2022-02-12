c5a0cecd314afe89aa27d1fd34b11f07f4234173
elhaw <abd.elsayed@hotmail.com>
2022-02-12 20:04:51 +0200

    added base site url env , meta data for seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import NextSeo, { DefaultSeo } from 'next-seo'
 import { Layout } from '@modules/shared/components/organisms'
 import '../styles/globals.css'
+import Head from 'next/head'
+
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
 }
@@ -11,22 +14,40 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+export default function MyApp({
+  Component,
+  pageProps,
+  router,
+}: AppPropsWithLayout) {
+  const { asPath } = router
+  const baseSiteUrl = process.env.NEXT_PUBLIC_BASE_URL
+  const isProduction = process.env.NEXT_PUBLIC_ENV === 'production'
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <style jsx global>{`
-        #__next {
-          display: flex;
-          flex-direction: column;
-          min-height: 100vh;
-        }
-        #__next main {
-          flex: 1;
-          overflow-x: hidden;
-          background-color: white;
-        }
-      `}</style>
-    </Layout>
+    <>
+      <Head>
+        <link rel="canonical" href={baseSiteUrl + asPath} />
+        <title>leader in professional furniture</title>
+        <meta name="description" content="description"></meta>
+        <meta
+          name="robots"
+          content={isProduction ? 'index,follow' : ' nofollow noindex'}
+        ></meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+        <style jsx global>{`
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+          #__next main {
+            flex: 1;
+            overflow-x: hidden;
+            background-color: white;
+          }
+        `}</style>
+      </Layout>
+    </>
   )
 }
