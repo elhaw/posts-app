b6ed963eebb28d0b0d833fc290f49a6945e58872
elhaw <abd.elsayed@hotmail.com>
2022-02-12 17:36:32 +0200

    added post and posts components

--- modules/posts/components/molecules/Post/index.tsx
+++ modules/posts/components/molecules/Post/index.tsx
@@ -1,16 +1,30 @@
 import { FC } from 'react'
+import NextImage from 'next/image'
 import { IPost } from './IPost'
-const Post: FC<IPost.IProps> = ({
-  id,
-  likes,
-  publishDate,
-  image,
-  tags,
-  text,
-}) => {
+const Post: FC<IPost.IProps> = ({publishDate, image, text }) => {
   return (
-    <div>
-      <h2>{id},{likes}</h2>
+    <div className="bg-white rounded-lg border border-gray-200 shadow-md ">
+      <NextImage
+        className="rounded-t-lg"
+        src={image}
+        alt={text}
+        loading="lazy"
+        layout="responsive"
+        objectFit="cover"
+        width="500"
+        height="300"
+      />
+      <div className="p-5 h-36 flex flex-col justify-between">
+        <h5 className="mb-2 text-normal font-bold tracking-tig  dark:text-black capitalize">
+          {text}
+        </h5>
+        <p className="dark:text-gray-700 text-sm">
+          Published at :{' '}
+          <span className="text-black tracking-light">
+            {new Date(publishDate).toUTCString()}
+          </span>
+        </p>
+      </div>
     </div>
   )
 }
--- modules/posts/components/organisms/Posts/index.tsx
+++ modules/posts/components/organisms/Posts/index.tsx
@@ -1,7 +1,26 @@
-import { FC } from 'react'
+import { FC, Fragment } from 'react'
+import Link from 'next/link'
+import { Post } from '../../molecules'
 import { IPosts } from './IPosts'
-const Posts: FC<IPosts.IProps> = () => {
-  return <div></div>
+const Posts: FC<IPosts.IProps> = ({ posts }) => {
+  return (
+   <>
+      {posts.map(({ image, text, publishDate, id }) => {
+        return (
+          <Link key={id} href={`/posts/${id}`}>
+            <a target="_blank">
+              <Post
+                image={image}
+                text={text}
+                publishDate={publishDate}
+                id={id}
+              />
+            </a>
+          </Link>
+        )
+      })}
+      </>
+  )
 }
 
 export default Posts
